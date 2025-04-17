e0fdd44e5083b0e08666f9eb6e8c9f2a56534eb4
Aditi Verma <anvil557@gmail.com>
2025-04-18 01:20:20 +0530

    base room and textures

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    rb
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
